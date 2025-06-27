599c5cd5baebbea0dc96af0feb2ffdef1fbc1fba
Nathan Whyte <nathanwhyte35@gmail.com>
2025-06-26 22:46:15 -0500

    Support multiple input files

--- ignore-existing-file.sh
+++ ignore-existing-file.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# TODO: get root path of repo
+
+# TODO: check for .gitingore and .git/info/exclude
+
+# TODO: check if file is already tracked
+
+declare -a FILES
+
+FILES_IDX=0
 for arg in "$@"; do
     if [ ! -f "$arg" ]; then
         echo "$arg not a file, skipping..."
@@ -9,12 +18,11 @@ for arg in "$@"; do
         continue
     fi
 
-    # TODO: get root path of repo
+    FILES[FILES_IDX]="$arg"
+    FILES_IDX=$((FILES_IDX + 1))
+done
 
-    # TODO: check for .gitingore and .git/info/exclude
-
-    # TODO: check if file is already tracked
-
+for arg in "${FILES[@]}"; do
     if grep -q "$arg" "$(pwd)"/.gitignore; then
         echo "$arg is already ignored, skipping..."
     fi
@@ -28,8 +36,10 @@ for arg in "$@"; do
     echo "$arg" >>"$(pwd)"/.git/info/exclude
 
     git add "$arg"
-    git commit -m "ingore-existing-file.sh: ignore $arg"
-    git show
+done
 
+git commit -m "ingore-existing-file.sh: ignore specific files"
+
+for arg in "${FILES[@]}"; do
     mv "$arg.ignore" "$arg"
 done
