70b902cd61a374a98300d69051ee702d3d96cff9
Nathan Whyte <nathanwhyte35@gmail.com>
2025-06-28 06:32:13 -0500

    Re-run formatter, add check if in Git repo

--- ignore-existing-file.sh
+++ ignore-existing-file.sh
@@ -1,45 +1,54 @@
 #!/bin/bash
 
-# TODO: get root path of repo
+GIT_DIR="$(git rev-parse --show-toplevel)"
+
+if [[ -z "$GIT_DIR" ]]; then
+  echo "Not in a git repository, exiting..."
+  exit 1
+fi
 
 # TODO: check for .gitingore and .git/info/exclude
 
 # TODO: check if file is already tracked
 
+# TODO: handle directories
+
+# FEAT: interactive mode
+
 declare -a FILES
 
 FILES_IDX=0
 for arg in "$@"; do
-    if [ ! -f "$arg" ]; then
-        echo "$arg not a file, skipping..."
-        continue
-    elif [ -d "$arg" ]; then
-        echo "$arg is a directory, skipping..."
-        continue
-    fi
+  if [ ! -f "$arg" ]; then
+    echo "$arg not a file, skipping..."
+    continue
+  elif [ -d "$arg" ]; then
+    echo "$arg is a directory, skipping..."
+    continue
+  fi
 
-    FILES[FILES_IDX]="$arg"
-    FILES_IDX=$((FILES_IDX + 1))
+  FILES[FILES_IDX]="$arg"
+  FILES_IDX=$((FILES_IDX + 1))
 done
 
 for arg in "${FILES[@]}"; do
-    if grep -q "$arg" "$(pwd)"/.gitignore; then
-        echo "$arg is already ignored, skipping..."
-    fi
+  if grep -q "$arg" "$(pwd)"/.gitignore; then
+    echo "$arg is already ignored, skipping..."
+  fi
 
-    if grep -q "$arg" "$(pwd)"/.git/info/exclude; then
-        echo "$arg is already ignored, skipping..."
-    fi
+  if grep -q "$arg" "$(pwd)"/.git/info/exclude; then
+    echo "$arg is already ignored, skipping..."
+  fi
 
-    mv "$arg" "$arg.ignore"
+  mv "$arg" "$arg.ignore"
 
-    echo "$arg" >>"$(pwd)"/.git/info/exclude
+  echo "$arg" >>"$(pwd)"/.git/info/exclude
 
-    git add "$arg"
+  git add "$arg"
 done
 
 git commit -m "ingore-existing-file.sh: ignore specific files"
 
 for arg in "${FILES[@]}"; do
-    mv "$arg.ignore" "$arg"
+  mv "$arg.ignore" "$arg"
 done
